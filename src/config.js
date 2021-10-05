43f000d635084a796f13040039deba50c14f5214
dp160610 <46530061+dp160610@users.noreply.github.com>
2021-10-05 10:05:15 +0530

    updates

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_URL = "https://api.themoviedb.org/3/";
-const API_KEY = "b0b2d67697ac5d805d43e95cda0d1b45";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
